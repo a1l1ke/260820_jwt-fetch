59e6763c57b03422b711a3b610fb1e73038f38ee
a1l1ke <onetenthplease@gmail.com>
2026-08-20 10:49:09 +0900

    feat: 회원가입 기능 구현(DTO, 패스워드 암호화, 폼 뷰) 및 JJWT 의존성/설정 추가

--- src/main/java/org/example/jwtfetch/config/AuthProperties.java
+++ src/main/java/org/example/jwtfetch/config/AuthProperties.java
@@ -2,8 +2,15 @@ package org.example.jwtfetch.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import java.time.Duration;
+
 @ConfigurationProperties(prefix = "app.auth")
 public record AuthProperties(
-        String encodingId
+        String encodingId,
+        Jwt jwt
 ) {
+    public record Jwt(
+            String secretKey,
+            Duration accessTokenExpiry) {
+    }
 }
